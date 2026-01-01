aac98b95d7dee9897ceb6469d374bd65c5be1564
Dawid Oleksiuk <oleksiukdawid@gmail.com>
2026-01-01 15:52:34 +0100

    adding test file

---
+++ dawid/test.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doleksiu <doleksiu@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/01 15:43:38 by doleksiu          #+#    #+#             */
+/*   Updated: 2026/01/01 15:43:38 by doleksiu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main()
+{
+	char *line;
+	line = readline(NULL);
+	printf("%s", line);
+}
