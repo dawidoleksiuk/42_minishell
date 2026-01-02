1d76f6157dd61ce6790d2b7653be3ce8328d94d8
Dawid Oleksiuk <oleksiukdawid@gmail.com>
2026-01-02 22:47:24 +0100

    testing signals handling

--- dawid/test.c
+++ dawid/test.c
@@ -6,23 +6,64 @@
 /*   By: doleksiu <doleksiu@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 15:43:38 by doleksiu          #+#    #+#             */
-/*   Updated: 2026/01/01 15:43:38 by doleksiu         ###   ########.fr       */
+/*   Updated: 2026/01/02 22:45:20 by doleksiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "./test.h"
 
-int main()
+void	sig_handler(int sig)
 {
-	char *line;
+	g_signum = sig;
+	if (sig == SIGINT)
+	{
+		printf("lala");
+		// rl_on_new_line();
+	}
+	if (sig == SIGQUIT)
+	{
+		printf("papa");
+	}
+}
+
+int	config_sigaction(struct sigaction *sa)
+{
+	bzero(sa, sizeof(sa));
+	if ((sigaddset(&sa->sa_mask, SIGINT) == -1) 
+		|| (sigaddset(&sa->sa_mask, SIGQUIT) == -1))
+		return (1);
+	sa->sa_handler = &sig_handler;
+	return (0);
+}
+
+int	signals(void)
+{
+	struct sigaction	sa;
+	if (config_sigaction(&sa) == 1)
+		return (1);
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return (1);
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
+		return (1);
+	return (0);
+}
+
+void	prompt(void)
+{
+	char	*line;
+
+	line = readline("minishell$ ");
+	add_history(line);
+	printf("%s \n", line);
+	free (line);
+}
+
+int	main(void)
+{
+	if (signals() == 1)
+		return (1);
 	while (1)
 	{
-		line = readline("minishell$ ");
-		add_history(line);
-		printf("%s \n", line);
-		free (line);
+		prompt();
 	}
 }
---
+++ dawid/test.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doleksiu <doleksiu@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 20:46:18 by doleksiu          #+#    #+#             */
+/*   Updated: 2026/01/02 22:20:33 by doleksiu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_H
+# define TEST_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <strings.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+// # include <sys/types.h>
+# include <unistd.h>
+
+volatile sig_atomic_t	g_signum;
+
+
+#endif
