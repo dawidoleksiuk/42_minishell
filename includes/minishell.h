64348a7fc6728c448251a317e874bf70d46a88c0
alusnia <adam.lusnia@gmail.com>
2026-01-05 21:19:04 +0100

    another

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: alusnia <alusnia@student.42Warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 15:40:44 by doleksiu          #+#    #+#             */
-/*   Updated: 2026/01/05 19:25:58 by alusnia          ###   ########.fr       */
+/*   Updated: 2026/01/05 20:10:58 by alusnia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 # define MINISHELL_H
 # include "dawid/tokenizer.h"
 # include "libft/libft.h"
+# include "gnl/get_next_line.h"
+# include "errno.h"
+
+typedef struct s_fd
+{
+	int	in;
+	int	pipe_fd[2];
+	int	out;
+} t_fd;
+
 
 typedef struct s_data
 {
