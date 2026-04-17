2c24b262758988015c0422ed3a20a13c9dd6d984
Dawid Oleksiuk <oleksiukdawid@gmail.com>
2026-04-17 12:09:48 +0200

    minor norm update for parser.c

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: doleksiu <doleksiu@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 17:38:03 by doleksiu          #+#    #+#             */
-/*   Updated: 2026/04/16 20:18:02 by doleksiu         ###   ########.fr       */
+/*   Updated: 2026/04/17 12:08:52 by doleksiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	parser(t_data *data)
 		if ((token && token->type == PIPE) || current_cmd == NULL)
 		{
 			current_cmd = new_cmd_node(data, current_cmd);
-			if (token->type == PIPE)
+			if (token && token->type == PIPE)
 				token = token->next;
 		}
 		if (token && token->type == WORD)
